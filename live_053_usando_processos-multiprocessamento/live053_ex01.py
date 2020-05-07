060c4bb97e4ddfc9d163a23bfd0b251f59ed3bd3
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-06 23:53:55 -0300

    Live 053 - criada a variavel pstate e alterada a funçao info.

--- live_053_usando_processos-multiprocessamento/live053_ex01.py
+++ live_053_usando_processos-multiprocessamento/live053_ex01.py
@@ -1,9 +1,14 @@
 """Exemplo de como compartilhar uma queue entre processos e threads."""
+from collections import namedtuple
 from threading import Thread
 from os import getpid, getppid
 
+pstate = namedtuple('psstate', 'name pid ppid')
+
+
 def info(name):
-    print(name, getpid(), getppid())
+    obj = pstate(name, getpid(), getppid())
+    print(obj)
 
 
 print(info('main line'))
