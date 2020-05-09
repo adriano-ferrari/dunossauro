f518044e81bcf0d0cca2576ebe93a8751761acb6
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-09 20:16:17 -0300

    Criado o arquivo live054_ex01.py

---
+++ live_054_concorrencia_usando_futures-multiprocessamento/live054_ex01.py
@@ -0,0 +1,9 @@
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+from requests import get
+
+
+l_urls = ['https://google.com'] * 6
+
+executor = ThreadPoolExecutor(max_workers=3)
+result = executor.map(get, l_urls)
+print(list(result))
