0d2236c0f592e6ae77848ec338cc8b2c1dfada89
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-09 20:19:00 -0300

    Primeira alteração no arquivo live054_ex01.py

--- live_054_concorrencia_usando_futures-multiprocessamento/live054_ex01.py
+++ live_054_concorrencia_usando_futures-multiprocessamento/live054_ex01.py
@@ -4,6 +4,10 @@ from requests import get
 
 l_urls = ['https://google.com'] * 6
 
-executor = ThreadPoolExecutor(max_workers=3)
-result = executor.map(get, l_urls)
-print(list(result))
+# executor = ThreadPoolExecutor(max_workers=3)
+# result = executor.map(get, l_urls)
+# print(list(result))
+
+with ThreadPoolExecutor(max_workers=3) as executor:
+    result = executor.map(get, l_urls)
+    print(result)
