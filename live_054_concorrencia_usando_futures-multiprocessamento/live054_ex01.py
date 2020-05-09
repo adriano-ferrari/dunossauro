9cd809366b73bc22450f2dc525914d1b3d427b52
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-09 20:25:58 -0300

    Segunda alteração no arquivo live054_ex01.py

--- live_054_concorrencia_usando_futures-multiprocessamento/live054_ex01.py
+++ live_054_concorrencia_usando_futures-multiprocessamento/live054_ex01.py
@@ -8,10 +8,15 @@ l_urls = ['https://google.com'] * 6
 # result = executor.map(get, l_urls)
 # print(list(result))
 
+
+
+print('Threads')
 with ThreadPoolExecutor(max_workers=3) as executor:
     """
     executor.__enter__ -> self (ThreadPoolExecutor)
     executor.__exit__ -> executor.shutdown (wait= True)
     """
-    result = executor.map(get, l_urls)
+    result = executor.map(get, l_urls, timeout=0.01)
     print(result)
+
+print(list(result))
