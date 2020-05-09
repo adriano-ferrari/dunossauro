892bb63e37580cfcd2c7debc31f93a17f4409403
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-09 20:46:59 -0300

    1ª alteração no arquivo live054_ex02.py

--- live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
+++ live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
@@ -1,6 +1,9 @@
 from concurrent.futures import ThreadPoolExecutor
+from time import sleep
 
 
 with ThreadPoolExecutor(max_workers=3) as exe:
+    result_0 = exe.submit(sleep, 10)
     result_1 = exe.submit(print, 'oi')
+    print(result_0)
     print(result_1)
