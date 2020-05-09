0a66f6321061c672678347997765761b2a537951
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-09 20:49:51 -0300

    2ª alteração no arquivo live054_ex02.py

--- live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
+++ live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
@@ -2,7 +2,12 @@ from concurrent.futures import ThreadPoolExecutor
 from time import sleep
 
 
-with ThreadPoolExecutor(max_workers=3) as exe:
+with ThreadPoolExecutor(max_workers=1) as exe:
+    """
+    GIL: tarefas bloqueantes de I/O.
+
+    Darão bypass no GIL
+    """
     result_0 = exe.submit(sleep, 10)
     result_1 = exe.submit(print, 'oi')
     print(result_0)
