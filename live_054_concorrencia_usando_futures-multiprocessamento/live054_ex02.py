e117164574ee9a7955523485444d2aa327ea2168
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-09 21:06:41 -0300

    4ª alteração no arquivo live054_ex02.py

--- live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
+++ live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
@@ -1,5 +1,6 @@
 from concurrent.futures import ProcessPoolExecutor
 from time import sleep
+from functools import partial
 
 
 with ProcessPoolExecutor() as exe:
@@ -8,7 +9,8 @@ with ProcessPoolExecutor() as exe:
 
     Darão bypass no GIL
     """
-    result_0 = exe.submit(sleep, 10)
+    result_0 = exe.submit(sleep, 5)
     result_1 = exe.submit(print, 'oi')
     print(result_0)
     print(result_1)
+    result_0.add_done_callback(partial(print, 'acabei'))
