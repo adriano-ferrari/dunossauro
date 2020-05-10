61ca57d54850c36eead98b8b6871341389bc5e30
adriano-ferrari <adriano.ferrari@aluno.faculdadeimpacta.com.br>
2020-05-09 21:08:49 -0300

    5ª alteração no arquivo live054_ex02.py

--- live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
+++ live_054_concorrencia_usando_futures-multiprocessamento/live054_ex02.py
@@ -13,4 +13,7 @@ with ProcessPoolExecutor() as exe:
     result_1 = exe.submit(print, 'oi')
     print(result_0)
     print(result_1)
+    while result_0.running():
+        print('Não acabei')
+        sleep(0.5)
     result_0.add_done_callback(partial(print, 'acabei'))
